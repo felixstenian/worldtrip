3959aa7e0ff6ca39a0d2b63af5e82b46f572b001
felixstenian <felixstenian@gmail.com>
2022-01-20 19:22:46 -0300

    update colors theme

--- styles/theme.ts
+++ styles/theme.ts
@@ -2,17 +2,10 @@ import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
   colors: {
-    gray: {
-      '900': '#181B23',
-      '800': '#1F2029',
-      '700': '#353646',
-      '600': '#4B4D63',
-      '500': '#616480',
-      '400': '#797D9A',
-      '300': '#9699B0',
-      '200': '#B3B5C6',
-      '100': '#D1D2DC',
-      '50': '#EEEEF2',
+    text: {
+      'light': '#F5F8FA',
+      'dark': '#47585B'
+
     }
   },
   fonts: {
@@ -22,8 +15,7 @@ export const theme = extendTheme({
   styles: {
     global: {
       body: {
-        bg: 'gray.900',
-        color: 'gray.50'
+        color: 'text.dark'
       }
     }
   }
