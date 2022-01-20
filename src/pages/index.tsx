ed4359201605baa7990d1150ce998f226a73c494
felixstenian <felixstenian@gmail.com>
2022-01-20 19:16:26 -0300

    create project

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Flex justify='center'>
+      <Text fontSize={32} fontWeight={700}>World trip</Text>
+    </Flex> 
+  )
+}
